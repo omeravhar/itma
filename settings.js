9894e8c846822fccdb08faebb2709c4228c57d1e
omer avhar <avharomer@gmail.com>
2019-06-10 16:05:22 +0300

    reading txt file and reading settings.js file for 1.html only

---
+++ settings.js
@@ -0,0 +1,15 @@
+//you can change the number and than save the file and refresh the page with F5 on the keybord
+// this is left or right of the circle element  - up for right , down for left
+document.getElementsByClassName('circle-container')[0].style.left='35%'; 
+
+
+//this up and down for circle element  - numbers down for getting the element up , numbers up for geting the element down 
+document.getElementsByClassName('circle-container')[0].style.top='30%'; 
+
+
+
+document.addEventListener('keydown', function(e) {
+      
+  if (e.key === 's')
+    alert('settings');
+})
